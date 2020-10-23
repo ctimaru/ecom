ff430b2148d6f798974c7c46dd57969a6f7d5c01
YOUR_USERNAME <c.timaru@gmail.com>
2020-10-23 10:43:43 +0200

    updating the app with new components

--- src/App.js
+++ src/App.js
@@ -6,18 +6,14 @@ import './App.css';
 
 import HomePage from './pages/homepage/hompage.component.jsx';
 
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-);
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
     <div className="App">
     <Switch>
       <Route exact path='/' component={HomePage} />
-      <Route path='/hats' component={HatsPage} />
+      <Route path='/shop' component={ShopPage} />
    </Switch>
     </div>
   );

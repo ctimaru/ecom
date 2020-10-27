420320e6a52f24963390b364d2768ca4ca5d261f
YOUR_USERNAME <c.timaru@gmail.com>
2020-10-27 10:50:03 +0100

    adding more components and setting firebase

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,26 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = {
+    apiKey: "AIzaSyC60hnj1cYuQiTS3UbqaITZ9sGXW33FoK8",
+    authDomain: "iv-db-6064a.firebaseapp.com",
+    databaseURL: "https://iv-db-6064a.firebaseio.com",
+    projectId: "iv-db-6064a",
+    storageBucket: "iv-db-6064a.appspot.com",
+    messagingSenderId: "225961785738",
+    appId: "1:225961785738:web:480f56f69a981c22cf7073",
+    measurementId: "G-WHK0VPHFLV"
+  }
+
+  firebase.initializeApp(config);
+
+  export const auth = firebase.auth();
+  export const firestore = firebase.firestore();
+  
+  const provider = new firebase.auth.GoogleAuthProvider();
+  provider.setCustomParameters({ prompt: 'select_account' });
+  export const signInWithGoogle = () => auth.signInWithPopup(provider);
+  
+  export default firebase;
+

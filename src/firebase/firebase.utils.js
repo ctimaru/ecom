1e0d0dde8736975c8bab2c7b506ab4c7ada65c59
YOUR_USERNAME <c.timaru@gmail.com>
2020-10-28 22:07:26 +0100

    added sign in and sign up and set firebase

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,17 +3,43 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-    apiKey: "AIzaSyC60hnj1cYuQiTS3UbqaITZ9sGXW33FoK8",
-    authDomain: "iv-db-6064a.firebaseapp.com",
-    databaseURL: "https://iv-db-6064a.firebaseio.com",
-    projectId: "iv-db-6064a",
-    storageBucket: "iv-db-6064a.appspot.com",
-    messagingSenderId: "225961785738",
-    appId: "1:225961785738:web:480f56f69a981c22cf7073",
-    measurementId: "G-WHK0VPHFLV"
-  }
+  apiKey: "AIzaSyC60hnj1cYuQiTS3UbqaITZ9sGXW33FoK8",
+  authDomain: "iv-db-6064a.firebaseapp.com",
+  databaseURL: "https://iv-db-6064a.firebaseio.com",
+  projectId: "iv-db-6064a",
+  storageBucket: "iv-db-6064a.appspot.com",
+  messagingSenderId: "225961785738",
+  appId: "1:225961785738:web:480f56f69a981c22cf7073",
+  measurementId: "G-WHK0VPHFLV"
+}
 
-  firebase.initializeApp(config);
+
+export const createUserProfileDocument= async (userAuth, additionalData) => { 
+  if(!userAuth) return;
+  
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+  
+  const snapShot = await userRef.get();
+  
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    
+    try{
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      });
+    } catch(error) {
+      console.log('Error creating user', error.message);
+    }
+  }
+  return userRef;
+}
+
+firebase.initializeApp(config);
 
   export const auth = firebase.auth();
   export const firestore = firebase.firestore();
@@ -23,4 +49,3 @@ const config = {
   export const signInWithGoogle = () => auth.signInWithPopup(provider);
   
   export default firebase;
-

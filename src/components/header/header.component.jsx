e0161ca73146003a21d9ba14dd9e73bceaf53902
YOUR_USERNAME <c.timaru@gmail.com>
2020-10-30 10:36:28 +0100

    adding redux

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
-
 import {Link} from 'react-router-dom';
+import {connect} from 'react-redux';
 
 import {auth} from '../../firebase/firebase.utils';
 
+import {ReactComponent as Logo} from '../../assets/crown.svg';
+
 import './header.styles.scss';
 
-import {ReactComponent as Logo} from '../../assets/crown.svg';
 
 const Header = ({currentUser}) => (
     <div className="header">
@@ -32,4 +33,8 @@ const Header = ({currentUser}) => (
     </div>
 )
 
-export default Header;
+const mapStateToProps = state => ({
+    currentUser: state.user.currentUser
+});
+
+export default connect(mapStateToProps)(Header);
